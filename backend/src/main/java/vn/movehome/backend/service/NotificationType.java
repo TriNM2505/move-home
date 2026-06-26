06076b45874a17c1be316a9ef7385a5055da22b0
Ngo Quoc Huy <ngohuy4898@gmail.com>
2026-06-26 16:15:24 +0700

    them 2 loai thong bao cho duyet tai xe

--- backend/src/main/java/vn/movehome/backend/service/NotificationType.java
+++ backend/src/main/java/vn/movehome/backend/service/NotificationType.java
@@ -6,6 +6,8 @@ public final class NotificationType {
     public static final String ORDER_IN_PROGRESS = "ORDER_IN_PROGRESS";
     public static final String ORDER_COMPLETED = "ORDER_COMPLETED";
     public static final String ORDER_CANCELLED = "ORDER_CANCELLED";
+    public static final String DRIVER_APPROVED = "DRIVER_APPROVED";
+    public static final String DRIVER_REJECTED = "DRIVER_REJECTED";
     public static final String WITHDRAWAL_PROCESSED = "WITHDRAWAL_PROCESSED";
     public static final String WITHDRAWAL_REJECTED = "WITHDRAWAL_REJECTED";
 
